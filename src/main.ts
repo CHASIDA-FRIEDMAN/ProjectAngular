05f651d6e78bb1ddbb44af5ed6e61f9634c68aa4
chasida friedman <chasida067@gmail.com>
2025-06-23 01:41:49 +0300

    login

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
-import {routes} from './app/app.routes'
+import { routes } from './app/app.routes'
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers:[provideRouter(routes)]
+  providers: [provideRouter(routes),
+  provideHttpClient()
+
+  ]
+
 }
 );
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../shared/services/user.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class LoginComponent {
+  loginData = {
+    email: '',
+    password: ''
+  };
 
+  loginError = '';
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  onSubmit(form: any) {
+    if (form.invalid) {
+      return
+    }
+
+    this.userService.signin(this.loginData).subscribe({
+      next: (res) => {
+        // הכניסה הצליחה, ניווט לדף המתכונים
+        this.loginError = '';
+        this.router.navigate(['/recipes']);
+      },
+      error: () => {
+        // הכניסה נכשלה, הצגת הודעת שגיאה
+        this.loginError = 'שם משתמש או סיסמה שגויים';
+      }
+    })
+  }
+
+  goToRegister() {
+    // ניווט לדף ההרשמה
+    this.router.navigate(['/register'], {
+      state: { email: this.loginData.email, password: this.loginData.password }
+    })
+  }
 }
