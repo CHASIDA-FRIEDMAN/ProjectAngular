76e8b988ebb09f948e49aef0cf7ebd7c84b20e29
chasida friedman <chasida067@gmail.com>
2025-06-23 15:49:36 +0300

    register

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { UserService } from '../../shared/services/user.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,CommonModule,HttpClientModule],
+  imports: [FormsModule,
+    CommonModule, HttpClientModule, MatCardModule, MatFormFieldModule,
+    MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
@@ -40,10 +49,13 @@ export class LoginComponent {
     })
   }
 
+  // פונקציה לניווט לדף ההרשמה עם פרטי המשתמש
   goToRegister() {
-    // ניווט לדף ההרשמה
-    this.router.navigate(['/register'], {
-      state: { email: this.loginData.email, password: this.loginData.password }
-    })
+  const email = this.loginData?.email || '';
+  const password = this.loginData?.password || '';
+  sessionStorage.setItem('registerEmail', email);
+  sessionStorage.setItem('registerPassword', password);
+  this.router.navigate(['/register']);
   }
+
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../shared/services/user.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  standalone:true,
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule,
+    MatButtonModule, ReactiveFormsModule, CommonModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class RegisterComponent {
+  private fb = inject(FormBuilder);
+  private userService = inject(UserService);
+  private router = inject(Router);
 
+  registerForm!: FormGroup;
+  errorMessage: string = '';
+
+  ngOnInit(): void {
+    // קבלת פרטי המשתמש מהניווט הקודם (אם קיימים)
+    // const navigation = this.router.getCurrentNavigation();
+    // const state = navigation?.extras.state as { email?: string, password?: string };
+    const email = sessionStorage.getItem('registerEmail') || '';
+    const password = sessionStorage.getItem('registerPassword') || '';
+
+    // יצירת הטופס עם הערכים המתקבלים (אם קיימים) בצורה שך reactive form
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: [email || '', [Validators.required, Validators.email]],
+      password: [password || '', [Validators.required, Validators.minLength(8)]],
+      address: ['', [Validators.required]],
+      role: ['registered_user']
+    })
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+    const userData = this.registerForm.value;
+    this.userService.signup(userData).subscribe({
+      next: (res) => {
+        // ההרשמה הצליחה, ניווט לדף הכניסה
+        // שמירה ב localStorage
+        localStorage.setItem('user', JSON.stringify(res));
+        this.router.navigate(['/recipes']);
+      },
+      error: (err) => {
+        console.error('Registration error:', err);
+        // ההרשמה נכשלה, הצגת הודעת שגיאה
+        this.errorMessage = err.error?.message || 'שגיאה בעת ההרשמה';
+      }
+    });
+  }
 }
